6015e06e492e68a8fabb52611d77f184a0d59cd6
fsdj20_31 <fsdj20_31@SEEDINFOTECH.COM>
2018-11-21 15:53:53 +0530

    master created custom component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
-
+import { NewcompComponent } from './newcomp/newcomp.component';
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    NewcompComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/newcomp/newcomp.component.ts
@@ -0,0 +1,19 @@
+ import { Component, OnInit} from '@angular/core';
+
+
+ @Component({
+    selector : 'app-newcomp',
+    styleUrls  : ['./newcomp.component.css'],
+    templateUrl : './newcomp.component.html'
+
+
+ })
+
+ export class NewcompComponent implements OnInit{
+     ngOnInit(): void {
+         throw new Error("Method not implemented.");
+     }
+
+
+ }
+
