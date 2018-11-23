b0a2d82b355c9ebd83643e1ac6926a9febc1b83a
fsdj20_31 <fsdj20_31@SEEDINFOTECH.COM>
2018-11-23 10:58:41 +0530

    master added directive ngFor

--- src/app/directivess/directivess.component.ts
+++ src/app/directivess/directivess.component.ts
@@ -7,8 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectivessComponent implements OnInit {
   status:boolean = false;
-  constructor() {  
+  employee:any[];
    
+  constructor() {  
+
+    
+   this.employee= [
+     
+    { code: 101, name : 'shubham', job : 'Angular Developer' },
+    { code: 102, name : 'Anuja', job : 'Android Developer' },
+    { code: 107, name : 'Shifa', job : 'Java Developer' },
+    { code: 109, name : 'Alia', job : 'Software Tester' },
+    { code: 105, name : 'Vasundhara', job : 'Calculator' }                
+  
+  
+  
+  
+  ]
 }
 
   ngOnInit() {
@@ -16,14 +31,18 @@ export class DirectivessComponent implements OnInit {
  
 
   }
+
+
   public test():void {
     if (this.status == false) {
       this.status = true;
     }
-    else{
+    else{  
 
       this.status = false;
 
     }
   }
+
+
 }
